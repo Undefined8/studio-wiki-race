7923df6bd0cd683d71a1fc34510a5960ba19bd31
undefined8 <dzamilatskiy2019@bklawtech.com>
2018-03-05 23:28:19 +0000

    Added Summary

---
+++ src/GameView.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import WikiApi from './WikiApi.js'
+
+export default class GameView extends Component {
+  
+  constructor(props){
+    super(props);
+    this.state = {
+      summary: "loading..."
+    }
+  }
+  
+  componentDidMount() {
+    var self = this;
+    WikiApi.getSummary(this.props.title).then(
+    function(text) {
+      self.setState({
+        summary: text
+      });
+    });
+  }
+
+  render() {
+    var currentTitle = this.props.title;
+
+
+
+    return (
+      <div>
+        {this.state.summary}
+      </div>
+    );
+  }
+}
